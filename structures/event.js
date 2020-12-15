6786d13c95997ac2d56c512e1825c7757804f4c9
Paradise Bots <67729779+ParadiseBotList@users.noreply.github.com>
2020-12-15 15:55:10 -0700

    Update event.js

--- structures/event.js
+++ structures/event.js
@@ -2,11 +2,11 @@ const { readdirSync } = require("fs");
 const { join } = require("path");
 const filePath = join(__dirname, "..", "events");
 
-module.exports.run = (client) => {
+module.exports.run = (paradise_api) => {
     const eventFiles = readdirSync(filePath);
     for(const eventFile of eventFiles) {
         const event = require(`${filePath}/${eventFile}`);
         const eventName = eventFile.split(".").shift();
-        client.on(eventName, event.bind(null, client));
+        client.on(eventName, event.bind(null, paradise_api));
     }
 }
