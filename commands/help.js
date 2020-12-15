d8101be96c333c600a772b7d86d7731f9315a08f
Paradise Bots <67729779+ParadiseBotList@users.noreply.github.com>
2020-12-15 16:01:32 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require ('discord.js');
 
-module.exports.run = async (paradise_api, message, args) => {
+module.exports.run = async (client, message, args) => {
 
     let info_commands = [
         "``api.help`` - Displays this help message",
@@ -16,21 +16,21 @@ module.exports.run = async (paradise_api, message, args) => {
         let prefix = 'api.'
 
         let embed = new MessageEmbed()
-          .setAuthor('Paradise API Help', paradise_api.config.embed_image)
+          .setAuthor('Paradise API Help', client.config.embed_image)
           .setDescription('Here is a List of my Available Commands')
-          .setColor(paradise_api.config.embed_color)
+          .setColor(client.config.embed_color)
           .addField('Information', info_commands)
           .addField('Paradise API', api_commands)
           .setTimestamp()
-          .setFooter(`Requested By: ${message.author.username}`, paradise_api.config.embed_image)
+          .setFooter(`Requested By: ${message.author.username}`, client.config.embed_image)
 
     if (!args[0]) {
         return message.channel.send(embed)
     }
 
-    if(args[0] && paradise_api.commands.has(args[0])) {
+    if(args[0] && client.commands.has(args[0])) {
 
-        const cmd = paradise_api.commands.get(args[0]);
+        const cmd = client.commands.get(args[0]);
 
         let cmdname = cmd.help.name.charAt(0).toUpperCase() + cmd.help.name.slice(1)
 
@@ -44,16 +44,16 @@ module.exports.run = async (paradise_api, message, args) => {
         }
 
         let cmdEmbed = new MessageEmbed()
-            .setAuthor(`Command: ${cmdname}`, paradise_api.config.embed_image)
+            .setAuthor(`Command: ${cmdname}`, client.config.embed_image)
             .setDescription(`Here is some info on the ${cmdname} command.`)
-            .setColor(paradise_api.config.embed_color)
+            .setColor(client.config.embed_color)
             .addField('Prefix', prefix, true)
             .addField('Name', cmd.help.name, true)
             .addField('Description', cmd.help.description, true)
             .addField('Category', cmd.help.category, true)
             .addField('Example', cmd.help.example, true)
             .addField('Aliases', "``" + aliases + "``")
-            .setFooter('Syntax: <> = Required | [] = Optional', paradise_api.config.embed_image)
+            .setFooter('Syntax: <> = Required | [] = Optional', client.config.embed_image)
 
             return message.channel.send(cmdEmbed)
     }    
