a0e6aa6031e51d9fd7e1d874a437f59cb90cc794
Paradise Bots <67729779+ParadiseBotList@users.noreply.github.com>
2020-12-15 15:59:11 -0700

    Update command.js

--- structures/command.js
+++ structures/command.js
@@ -2,13 +2,13 @@ const { readdirSync } = require("fs")
 const { join } = require("path")
 const filePath = join(__dirname, "..", "commands");
 
-module.exports.run = (paradise_api) => {
+module.exports.run = (client) => {
     for (const cmd of readdirSync(filePath).filter(cmd => cmd.endsWith(".js"))) {
         const prop = require(`${filePath}/${cmd}`);
-        paradise_api.commands.set(prop.help.name, prop);
+        client.commands.set(prop.help.name, prop);
 
          if(prop.help.aliases) for (const alias of prop.help.aliases) {
-             paradise_api.aliases.set(alias, prop);
+             client.aliases.set(alias, prop);
          }
     }
 }
