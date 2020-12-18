e69d4c951b0d0c117ca82fac0aff1d1a607c8b01
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-12-18 05:10:43 -0700

    Upload invite command

---
+++ commands/invite.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require ('discord.js');
+
+module.exports.run = async (client, message, args) => {
+
+    message.delete().catch()
+
+    try {
+
+        let inviteEmbed = new MessageEmbed()
+        .setAuthor('Paradise API Invite', client.config.embed_image)
+        .setDescription('Interested in using me in your server? Invite me here!!')
+        .setColor(client.config.embed_color)
+        .addField('Invite Link', `[Click Me](${client.config.inviteLink})`)
+        .setTimestamp()
+        .setFooter(`Currently In: ${client.guilds.cache.array().length} Servers`)
+
+    } catch (e) {
+        let error = new MessageEmbed()
+        .setAuthor('(500) Internal Server Error', client.config.embed_image)
+        .setDescription('An error occured while executing this command!')
+        .setColor(client.config.embed_color)
+        .addField('Error', `${e.message}`)
+        .setTimestamp()
+        .setFooter('Yikes, Is it bad!', client.config.embed_image)
+    }
+}
+
+module.exports.help = {
+    name: 'invite',
+    category: 'Paradise API',
+    aliases: ['invite-bot'],
+    description: 'Sends you a invite link for Paradise API',
+    example: 'api.invite'
+}
+
+module.exports.requirements = {
+    userPerms: [],
+    clientPerms: ['EMBED_LINKS'],
+    ownerOnly: false
+}
+
+module.exports.limits = {
+    rateLimits: 2,
+    cooldown: ie4
+}
