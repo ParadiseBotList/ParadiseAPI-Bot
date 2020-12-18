7430fea2e732fbc62546e78deb3eb2853ecad791
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-12-18 04:00:35 -0700

    Update get-bot.js

--- commands/get-bot.js
+++ commands/get-bot.js
@@ -12,7 +12,7 @@ module.exports.run = async (client, message, args) => {
 
     message.delete().catch()
 
-    let bot_to_get = message.mentions.users.first();
+    let bot_to_get = message.mentions.users.first(); || client.users.cache.get(args[0]);
 
     if (!bot_to_get || !bot_to_get.bot) return message.channel.send('Please provide a bot that is listed on our website to getch info about');
 
